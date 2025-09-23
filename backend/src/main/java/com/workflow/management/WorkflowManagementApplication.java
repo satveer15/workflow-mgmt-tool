45c8599ee5284fd7237265e5901c162f5a120eec
Satveer Kaur <sukulotey@gmail.com>
2025-09-24 00:35:17 +0530

    Feat: Main application class, and spring boot

---
+++ backend/src/main/java/com/workflow/management/WorkflowManagementApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowManagementApplication.class, args);
+    }
+
+}
