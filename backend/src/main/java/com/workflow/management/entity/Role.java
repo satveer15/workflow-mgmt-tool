05cfa5b6ea03ab53ece226bc83fbf386f2f6ed2d
Satveer Kaur <sukulotey@gmail.com>
2025-09-24 15:16:17 +0530

    Init: entity connection and roles

---
+++ backend/src/main/java/com/workflow/management/entity/Role.java
@@ -0,0 +1,26 @@
+package com.workflow.management.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 20)
+    private RoleType name;
+
+    public Role(RoleType name) {
+        this.name = name;
+    }
+}
