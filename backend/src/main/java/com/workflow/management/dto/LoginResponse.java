84454a7163c91902cc38bd6de0c19baf0ac744e3
Satveer Kaur <sukulotey@gmail.com>
2025-09-25 21:41:17 +0530

    Init: create auth dto loginRequest, loginResponse, registerRequest

---
+++ backend/src/main/java/com/workflow/management/dto/LoginResponse.java
@@ -0,0 +1,26 @@
+package com.workflow.management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private String tokenType = "Bearer";
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public LoginResponse(String token, String username, String email, List<String> roles) {
+        this.token = token;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+}
